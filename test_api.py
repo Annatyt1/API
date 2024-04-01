3b4ce9c124f1e7ebb6f4b85fc0f523e2dcf68556
Annatyt1 <165688320+Annatyt1@users.noreply.github.com>
2024-04-02 00:05:26 +0800

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -15,24 +15,23 @@ def test_success():
     assert response.status_code == 200 
 
 @allure.title('change animal_type')
-@allure.description('change animal_type, it should retern error status_code')
+@allure.description('change animal_type, it should retern status_code 200')
 def test_fail():
-    param = {'animal_type': 'horse', 'amount': '1'}
-    url = 'https://cat-fact.herokuapp.com/facts/animal_type=cat&amount=1'
+    param = {'animal_type': "####", 'amount': '1'}
+    url = 'https://cat-fact.herokuapp.com/facts'
 
     response = requests.get(url, params=param)
 
     assert response.content is not None
-    assert response.status_code == 400
+    assert response.status_code == 200
 
 @allure.title('change amount')
-@allure.description('change amount, it should retern error status_code')
+@allure.description('change amount, it should retern status_code 200')
 def test_fail_1():
     param = {'animal_type': 'cat', 'amount': '0'}
-    url = 'https://cat-fact.herokuapp.com/facts/animal_type=cat&amount=1'
+    url = 'https://cat-fact.herokuapp.com/facts'
 
     response = requests.get(url, params=param)
 
     assert response.content is not None
-    assert response.status_code == 400
-
+    assert response.status_code == 200
